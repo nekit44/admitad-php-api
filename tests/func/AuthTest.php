3b6a1821d52e0c4f668fa2d2d92ac51d5be9a175
Nikita Kobylin <nekit81@ya.ru>
2023-03-22 23:56:52 +0300

    base auth test

---
+++ tests/func/AuthTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace func\test;
+
+use Nekit44\AdmitadPhpApi\Api;
+use PHPUnit\Framework\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function testDoesntExistClientId()
+    {
+        $api = new Api();
+        $clientId = 'cb281d918a37e346b45e9aea1c6eb7';
+        $secret = 'a0f8a8b24de8b8182a0ddd2e89f5b1';
+        $this->expectExceptionMessage("Failed: client_id cb281d918a37e346b45e9aea1c6eb7 doesn't exist");
+        $api->authorizedByClientIdAndSecret($clientId, $secret, 'advcampaigns banners websites');
+    }
+
+}
