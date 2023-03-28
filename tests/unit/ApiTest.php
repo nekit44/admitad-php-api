b990101636ce43c86dbd68904229bf086c6b27ad
Nikita Kobylin <nekit81@ya.ru>
2023-03-28 11:14:47 +0300

    refactor

--- tests/unit/ApiTest.php
+++ tests/unit/ApiTest.php
@@ -25,9 +25,9 @@ class ApiTest extends TestCase
     {
         $curl = new DummyCurl();
         $api = new Api('access_token', $curl);
-        $clientId = 'b5c3064c07f18c931f65a6c23d3880';
-        $secret = 'b98de2cbc0801976f1dcd47c3d61a2';
+        $clientId = 'cb281d918a37e346b45e9aea1c6eb7';
+        $secret = 'a0f8a8b24de8b8182a0ddd2e89f5b1';
         $api->authorizedByClientIdAndSecret($clientId, $secret, 'advcampaigns banners websites');
-        assertEquals('YjVjMzA2NGMwN2YxOGM5MzFmNjVhNmMyM2QzODgwOmI5OGRlMmNiYzA4MDE5NzZmMWRjZDQ3YzNkNjFhMg==', $api->getBasicToken());
+        assertEquals('Y2IyODFkOTE4YTM3ZTM0NmI0NWU5YWVhMWM2ZWI3OmEwZjhhOGIyNGRlOGI4MTgyYTBkZGQyZTg5ZjViMQ==', $api->getBasicToken());
     }
 }
